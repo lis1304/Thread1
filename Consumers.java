dbbedc92057187bfea4ef4191c0514e741bcc81a
lis1304 <isl1304@yandex.ru>
2018-01-23 08:37:08 +0400

    поток1

--- Consumers.java
+++ Consumers.java
@@ -8,12 +8,20 @@ public class Consumers extends Thread {
 
     @Override
     public void run() {
+
+
         while (true) {
             try {
+                Integer element = null;
 
-                wait();
-                System.out.println(Thread.currentThread().getName() + " Вытащил: " + Producer.queue.poll());
-                //Thread.sleep(1000);
+                element = Producer.queue.poll();
+                if (element == null){
+                    wait();
+                    Thread.sleep(1000);
+                    element = Producer.queue.poll();
+                    notify();
+                }
+                System.out.println(Thread.currentThread().getName() + " Вытащил: " + element);
 
             }
             catch (InterruptedException e) {
--- Producer.java
+++ Producer.java
@@ -23,8 +23,11 @@ public class Producer extends Thread{
         while (true){
 
             queue.add(random());
-            notify();
-            //Thread.sleep(500);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
     }
